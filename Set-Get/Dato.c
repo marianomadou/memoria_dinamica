e8c2256723fe5e381006ef9f6d01ef6923516890
marianomadou <marianomadou@gmail.com>
2018-05-29 18:06:57 -0300

    subido, clases

---
+++ Set-Get/Dato.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Dato.h"
+
+int eDato_set_a(eDato* this, int valor)
+{
+    int ret = 0;
+    if(this!=NULL && valor>1500)
+    {
+        this->a = valor;
+        ret = 1;
+    }
+    return ret;//si puede asignarlo retorna 1, sino 0
+}
+
+int eDato_set_b(eDato* dato, char* valor)
+{
+    int ret = 0;
+    if(dato!=NULL && valor!=NULL)
+    {
+        strcpy(dato->b , valor);
+        ret = 1;
+    }
+    return ret;
+}
+
+int eDato_get_a(eDato* dato)
+{
+    int valor = -1;
+    if(dato!=NULL)
+    {
+        valor = dato->a;
+    }
+    return valor;
+}
+
+char* eDato_get_b(eDato* dato)
+{
+    char* valor;
+    if(dato!=NULL)
+    {
+       valor=dato->b;
+    }
+    return valor;
+}
+
+
+
---
+++ Set-Get/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Dato.h"
+
+//GETer=obtiene un valor y SETer=Escribe un valor
+
+
+eDato* pedirMemoria();
+eDato* Memoria_parametros(int, char*);
+
+
+int main()
+{
+    //eDato dato;
+    eDato* d=NULL;
+    eDato* e;
+
+    //d=&dato;
+//    d=(eDato*) malloc(sizeof(eDato));
+
+    d=pedirMemoria();
+
+    if(d!=NULL)
+    {
+
+    eDato_set_a(d,1661);//practicar validaciones
+
+    eDato_set_b(d,"Peter");
+
+    printf("%d -- %s", eDato_get_a(d),eDato_get_b(d));
+
+    }
+
+    e=Memoria_parametros(15000, "Luis");
+
+    if(d!=NULL)
+        {
+           printf("%d -- %s", eDato_get_a(e),eDato_get_b(e));
+        }
+    return 0;
+}
+
+eDato* pedirMemoria()
+{
+eDato* retorn;
+
+if (retorn!=NULL)
+    {
+     retorn=(eDato*)malloc(sizeof(eDato));
+        }else
+        {
+         printf("No hay suficiente memoria libre ");
+         retorn=NULL;
+        }
+return retorn;
+}
+
+eDato* Memoria_parametros(int a, char* b)
+{
+    eDato* retorno;
+    retorno=pedirMemoria();
+
+    if (retorno!=NULL)
+    {
+     eDato_set_a(retorno,a);
+     eDato_set_b(retorno,b);
+    }
+return retorno;
+}
